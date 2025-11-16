fb620d6c3963d7065dcb1ebd110a1357346a28ab
Pelcran Tales <pelcrantales@gmail.com>
2025-11-16 12:37:15 +0100

    Adjusted logo position in splash screen

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -11,13 +11,10 @@ function SplashScreen() {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1.5 }}
-        className="w-32 sm:w-36 md:w-40 lg:w-48 mt-[20vh]" 
+        className="w-32 sm:w-36 md:w-40 lg:w-48 mt-[40vh]" 
       />
     </div>
   );
 }
 
 export default SplashScreen;
-
-
-
