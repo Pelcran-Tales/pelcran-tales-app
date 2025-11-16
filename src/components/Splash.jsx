7c034e345ef31b3f33c91d4cea71e2b5785e40a1
Pelcran Tales <pelcrantales@gmail.com>
2025-11-16 12:34:02 +0100

    Adjusted logo position in splash screen

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -4,19 +4,20 @@ import verticalLogo from '../assets/logo-vertical.svg';
 
 function SplashScreen() {
   return (
-    <div className="w-screen h-screen bg-[#F4EAD5] relative">
+    <div className="w-screen h-screen bg-[#F4EAD5] flex items-start justify-center">
       <motion.img
         src={verticalLogo}
         alt="Logo"
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1.5 }}
-        className="w-32 sm:w-36 md:w-40 lg:w-48 absolute left-1/2 top-[40%] -translate-x-1/2 -translate-y-1/2"
+        className="w-32 sm:w-36 md:w-40 lg:w-48 mt-[20vh]" 
       />
     </div>
   );
 }
 
 export default SplashScreen;
+
 
 
